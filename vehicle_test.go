15e16383fa70a6d003460167a502fdb615f30be9
Angelo Brouns <angelo_brouns@hotmail.com>
2019-02-14 14:55:50 +0100

    Added tests for VehicleExists() & QueryParamExists()

--- vehicle_test.go
+++ vehicle_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
 
-func Test_VehicleExists_Succes(t *testing.T) {
+func Test_VehicleExists_Exists(t *testing.T) {
 	vehicle := Vehicle{Licence: "11-ww-11"}
 	expected := true
 	actual := VehicleExists(vehicle)
@@ -10,7 +14,27 @@ func Test_VehicleExists_Succes(t *testing.T) {
 		t.Errorf("Test failed, expected: '%t', got: '%t'. Licence = %s", expected, actual, vehicle.Licence)
 	}
 }
+func Test_VehicleExists_DoesNotExist(t *testing.T) {
+	vehicle := Vehicle{}
+	expected := false
+	actual := VehicleExists(vehicle)
+	if actual != expected {
+		t.Errorf("Test failed, expected: '%t', got: '%t'. Licence = %s", expected, actual, vehicle.Licence)
+	}
+}
 
-func Test_QueryParamExists(t *testing.T) {
+func Test_QueryParamExists_Exists(t *testing.T) {
+	u, err := url.Parse("test.com/test?parameter=1")
+	if err != nil {
+		panic(err.Error)
+	}
+	request := http.Request{URL: u}
 
+	expectedString := "1"
+	expectedBool := true
+	actualString, actualBool := QueryParamExists("parameter", &request)
+	if actualBool != expectedBool || actualString != expectedString {
+		t.Errorf("Test failed, expected: '%t', '%s', got: '%t', '%s'",
+			expectedBool, expectedString, actualBool, actualString)
+	}
 }
